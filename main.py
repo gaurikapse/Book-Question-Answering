41ccde48e56434e48aad5aecc7c9aa879f24ac55
gaurikapse <gkapse08@gmail.com>
2023-12-23 13:40:16 +0400

    add gitignore, create directory structure

---
+++ main.py
@@ -0,0 +1,7 @@
+import qa_book_functions as qabf
+import gradio as gr
+import os
+
+
+if __name__ == '__main__':
+    pass
---
+++ qa_book_functions/__init__.py
@@ -0,0 +1,16 @@
+from langchain.memory import ConversationBufferMemory
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import MongoDBChatMessageHistory
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains.question_answering import load_qa_chain
+import re
+
+
+def dataset():
+    pass
+
+
+def chat():
+    pass
